41f4536cb8392f74d324cd881dedc0b60291398a
Hugo Seixas <tameirao.hugo@gmail.com>
2021-01-25 18:37:08 -0300

    Fill code header

--- 01_download.R
+++ 01_download.R
@@ -1,13 +1,19 @@
 # HEADER ----------------------------------------------------------------------
 #
-# Title:        
-# Description:  
+# Title:        Download layers
+# Description:  The script uses the GEE api from {rgee} to access, process
+#               and download the spatial data required to perform the 
+#               experiment.  
 #
 # Authors:      Hugo Tameirao Seixas, Marcelo Bandoria
 # Contact:      tameirao.hugo@gmail.com, marcelobandoria@gmail.com
 # Date:         2021-25-01
 #
-# Notes:        
+# Notes:        In order to run this routine, you will need to have access
+#               to Google Earth Engine (https://earthengine.google.com/).
+#               You will also need to install and configure the {rgee} package,
+#               Installation and use guide can be found in:
+#               https://r-spatial.github.io/rgee/        
 #
 # LIBRARIES -------------------------------------------------------------------
 #
