1179671837f7adaac4e792bff8663c7473c2a8cb
Hugo Seixas <hugo.seixas@alumni.usp.br>
2021-02-19 10:48:56 -0300

    Create config file

---
+++ config/config.R
@@ -0,0 +1,7 @@
+# CONFIG ----------------------------------------------------------------------
+
+## Set the focal distance between control and experiment cells ----
+focal_distance <- 30
+
+## Set the scale of the masks download
+scale <- 900
