4c6747c7accd5555358691053caee30156a9192a
Hugo Seixas <hugo.seixas@alumni.usp.br>
2021-02-19 10:48:44 -0300

    Rename file, add download script

--- analysis/01_download_masks.R
+++ analysis/01_download_masks.R
@@ -20,6 +20,8 @@
 library(geojson)
 library(geojsonsf)
 library(rgee)
+library(googledrive)
+library(fs)
 library(sf)
 library(geobr)
 library(dplyr)
@@ -27,7 +29,9 @@ library(purrr)
 #
 # OPTIONS ---------------------------------------------------------------------
 #
+source("config.R")
 gee_email <- "hugo.seixas@alumni.usp.br"
+sf::sf_use_s2(FALSE)
 #
 # START GEE API ---------------------------------------------------------------
 
@@ -39,7 +43,12 @@ ee_Initialize(email = gee_email, drive = TRUE)
 ## Load Mata Atlântica polygon ----
 biome <-
   read_biomes() %>%
-  filter(name_biome == "Mata Atlântica")
+  filter(name_biome == "Mata Atlântica") %>%
+  select(geom) %>%
+  st_cast("POLYGON") %>%
+  mutate(area = st_area(geom)) %>%
+  slice(which.max(area)) %>% # Get only the main polygon
+  st_geometry()
 
 # Upload to GEE
 biome <- sf_as_ee(biome)
@@ -83,13 +92,11 @@ prim_forest_mask <-
   )$
   reduce(ee$Reducer$allNonZero())
 
-# Expand primary forest mask
-expanded_mask <- prim_forest_mask$focal_max(radius = 10)
-
 ## Create secondary forest mask ----
 # Pixels that presented secondary forest at any year
 sec_forest_mask <-
-  ee$ImageCollection$fromImages(
+  ee$ImageCollection$
+  fromImages(
     map(
       sec_forest$bandNames()$getInfo(), 
       function(band_name) {
@@ -110,7 +117,69 @@ sec_forest_mask <-
 ## Create buffer between masks ----
 sec_forest_mask <- 
   sec_forest_mask$updateMask(
-    prim_forest_mask$focal_max(radius = 10)$
+    prim_forest_mask$
+      focal_max(radius = focal_distance)$
       subtract(prim_forest_mask)$
       eq(0)
   )
+
+# DOWNLOAD MASKS --------------------------------------------------------------
+
+## Set the download for the primary forests mask ----
+# Control cells
+download_prim_mask <-
+  ee_image_to_drive(
+    image = prim_forest_mask$clip(biome),
+    description = "prim_forest_mask",
+    folder = "atlantic-regeneration",
+    timePrefix = FALSE,
+    region = biome$bounds(),
+    scale = scale,
+    maxPixels = 1e13,
+    fileFormat = "GEO_TIFF",
+    crs = "EPSG:4326"
+  )
+
+## Set the download for the secondary forests mask ----
+# Experiment cells
+download_sec_mask <-
+  ee_image_to_drive(
+    image = sec_forest_mask$clip(biome),
+    description = "sec_forest_mask",
+    folder = "atlantic-regeneration",
+    timePrefix = FALSE,
+    region = biome$bounds(),
+    scale = scale,
+    maxPixels = 1e13,
+    fileFormat = "GEO_TIFF",
+    crs = "EPSG:4326"
+  )
+
+## Start downloads ----
+download_prim_mask$start()
+download_sec_mask$start()
+
+## Monitor downloads ----
+ee_monitoring(download_prim_mask, task_time = 30)
+ee_monitoring(download_sec_mask, task_time = 30)
+
+## Find files in google drive ----
+drive_files <- drive_ls(path = "atlantic-regeneration")
+
+## Create local download dir ----
+dir_create("data/masks")
+
+## Download files to project folder ----
+walk2(
+  .x = drive_files$id,
+  .y = drive_files$name,
+  function(id, name) {
+    
+    drive_download(
+      file = as_id(id),
+      path = glue::glue('data/masks/{name}'),
+      overwrite = TRUE
+    )
+    
+  }
+)
