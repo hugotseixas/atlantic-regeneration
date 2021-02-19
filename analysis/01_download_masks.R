49487ae207d4ffa92ef1c663fcd66bf808a1705b
Hugo Seixas <hugo.seixas@alumni.usp.br>
2021-02-19 10:49:38 -0300

    Fix config file path

--- analysis/01_download_masks.R
+++ analysis/01_download_masks.R
@@ -29,7 +29,7 @@ library(purrr)
 #
 # OPTIONS ---------------------------------------------------------------------
 #
-source("config.R")
+source("config/config.R")
 gee_email <- "hugo.seixas@alumni.usp.br"
 sf::sf_use_s2(FALSE)
 #
